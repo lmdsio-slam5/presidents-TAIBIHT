98ca9b310c47a1d546115f89c823d1a8af19d787
TAIBIHT <hilmy.taibi@gmail.com>
2015-12-02 09:34:47 +0100

    push

--- app/config/prod.php
+++ app/config/prod.php
@@ -1,12 +1,19 @@
 <?php
 
+// MySQL config for OpenShift
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbName = getenv('OPENSHIFT_GEAR_NAME');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+
 // Doctrine (db)
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => 'localhost',
-    'port'     => '3306',
-    'dbname'   => 'presidents',
-    'user'     => 'presidents_user',
-    'password' => 'secret',
+    'host'     => "$dbHost",
+    'port'     => "$dbPort",
+    'dbname'   => "$dbName",
+    'user'     => "$dbUser",
+    'password' => "$dbPassword",
 );
